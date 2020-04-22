1fdb75b0467c05b5559cf42a03bba1c3d42857e7
vibs98 <workvibhor@gmail.com>
2020-04-22 11:06:58 +0530

    Completed Assignment 2

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
+import { AssignmentComponent, Todo} from '../../assignment.component';
 
 @Component({
   selector: 'app-assignment2',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+  @Input()
+  todos: Todo[];
+
+  @Output()
+  inputChanged = new EventEmitter<string>();
+
+  clicked : string;
+
+  constructor(private assignmentComponent : AssignmentComponent) { }
+ 
+  ngOnInit(){
+     this.todos = this.assignmentComponent.todos;
+  }
+
+  inputClass(event){
+    event.target.parentNode.className = "active";
+  }
 
 }
